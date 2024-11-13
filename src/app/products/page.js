605ca4aef4233ae8d1953db8bd925f6360ee4a71
Maria <madr0002@stud.kea.dk>
2024-11-13 20:24:42 +0100

    søge funktion done

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -15,12 +15,26 @@ const fetcher = (url) => fetch(url).then((res) => res.json());
 const ProductsPage = () => {
   const { data, error } = useSWR("https://dummyjson.com/products", fetcher);
   const [showPopup, setShowPopup] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("")
   console.log(data);
 
   if (error) return <div>Der opstod en fejl...</div>;
   if (!data) return <div>Indlæser...</div>;
   // hvis `data` stadig er null betyder det, at dataene endnu ikke er hentet
   // viser indlæser besked indtil de er tilgængelige
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value.toLowerCase()); // Convert input to lowercase for case-insensitive comparison
+  };
+
+  const filteredProducts = data.products.filter((product) => {
+    const titleMatch = product.title.toLowerCase().includes(searchQuery);
+    const tagsMatch = product.tags.some((tag) =>
+      tag.toLowerCase().includes(searchQuery)
+    );
+    const brandMatch = product.brand && product.brand.toLowerCase().includes(searchQuery);
+
+    return titleMatch || tagsMatch || brandMatch;
+  });
 
   const handleAddToCart = () => {
     setShowPopup(true);
@@ -32,6 +46,16 @@ const ProductsPage = () => {
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-serif font-bold text-center mb-8 text-gray-900">Webshop</h1>
+      <div className="mb-6">
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={handleSearchChange}
+          placeholder="Søg efter produkt, brand eller tag"
+          className="w-full p-3 border rounded-md focus:outline-none focus:ring-2 focus:ring-pink-500"
+        />
+      </div>
+
       {showPopup && (
         <PopupNotification
           message="Produkt tilføjet til din kurv!"
@@ -40,8 +64,14 @@ const ProductsPage = () => {
       )}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
         {/* mapper dataen i products arrayet */}
-        {data.products.map((product) => (
-          <ProductCard key={uuidv4()} id={product.id} name={product.title} description={product.description} price={product.price} image={product.thumbnail} onAddToCart={handleAddToCart}/>
+        {filteredProducts.map((product) => (
+          <ProductCard 
+          key={uuidv4()} 
+          id={product.id} 
+          name={product.title} 
+          description={product.description} 
+          price={product.price} image={product.thumbnail} 
+          onAddToCart={handleAddToCart}/>
         ))}
       </div>
       
